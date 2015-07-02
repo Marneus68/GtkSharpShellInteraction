ee200bf7316528b31cbe7c71f797ed792736ba68
Duane Bekaert <duane.bekaert@gmail.com>
2015-07-02 17:19:04 +0200

    Initial commit.

---
+++ GtkSharpShellInteraction/CliRunner.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+
+namespace GtkSharpShellInteraction
+{
+	public class CliRunner
+	{
+		public static void Run(string exName, string arguments, int timeoutSeconds, Func<String, String> writer) {
+			StreamReader outputStream = StreamReader.Null;
+			string ret = "";
+			bool success = false;
+
+			try {
+				Process newProcess = new Process();
+				newProcess.StartInfo.FileName = exName;
+				newProcess.StartInfo.Arguments = arguments;
+				newProcess.StartInfo.UseShellExecute = false;
+				newProcess.StartInfo.CreateNoWindow = true;
+				newProcess.StartInfo.RedirectStandardOutput = true;
+				newProcess.Start();
+
+				if (0 == timeoutSeconds)
+				{
+					outputStream = newProcess.StandardOutput;
+					ret = outputStream.ReadToEnd();
+					newProcess.WaitForExit();
+				}
+				else
+				{
+					success = newProcess.WaitForExit(timeoutSeconds * 1000);
+
+					if (success)
+					{
+						outputStream = newProcess.StandardOutput;
+						ret = outputStream.ReadToEnd();
+					}
+					/*
+					else
+					{
+						ret = "Timed out at " + timeoutSeconds + " seconds waiting for " + exName + " to exit.";
+					}
+					*/
+				}
+
+			} catch(Exception e) {
+				//throw (new Exception ("An error occurred running " + exeName + ".", e));
+				Console.WriteLine("Exception: " + e.Message);
+			} finally {
+				outputStream.Close();
+			}
+
+			writer (ret);
+		}
+	}
+}
+	
---
+++ GtkSharpShellInteraction/MainWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window {
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel) {
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a) {
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void RunButtonClicked (object sender, EventArgs e) {
+		if (commandEntry.Text.Trim() != "") {
+			String command = commandEntry.Text.Trim ();
+
+			String[] splitCommand = command.Split (' ');
+
+			String exname = splitCommand [0];
+			String parameters = "";
+
+			foreach (var s in splitCommand) {
+				if (s == exname) continue;
+				parameters += " " + s;
+			}
+				
+			outputTextview.Buffer.Text += " \ud83d\udd39 " + commandEntry.Text + System.Environment.NewLine;
+			commandEntry.Text = "";
+
+			GtkSharpShellInteraction.CliRunner.Run (exname, parameters, 9001, (l) => {
+				System.Console.WriteLine(l);
+				// This is needed as I am about to do something in the UI thread
+				Gtk.Application.Invoke(delegate {outputTextview.Buffer.Text += l + System.Environment.NewLine;});
+				return "";
+			});
+			//ProcessHelpers.ThreadedProcessRunner.RunProcess (exname, parameters);
+			/*
+			ProcessHelpers.ThreadedProcessRunner.RunProcessWithOutputRedirection (exname, parameters, (l) => {
+				System.Console.WriteLine(l);
+				// This is needed as I am about to do something in the UI thread
+				Gtk.Application.Invoke(delegate {
+					outputTextview.Buffer.Text += l + System.Environment.NewLine;
+				});
+				return "";
+			});
+			*/
+		}
+	}
+
+	protected void CloseButtonClicked (object sender, EventArgs e) {
+		Application.Quit ();
+	}
+
+	protected void CommandEntryActivated (object sender, EventArgs e) {
+		RunButtonClicked (sender, e);
+	}
+}
